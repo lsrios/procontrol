501adb35ab0199222e7e3f74eef9674990d3657c
Lucas Rios <lsrios01@gmail.com>
2020-11-29 16:47:18 -0300

    Initial commit

---
+++ src/main/java/br/com/projuris/procontrol/ProcontrolApplication.java
@@ -0,0 +1,13 @@
+package br.com.projuris.procontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projuris/procontrol/ProcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projuris.procontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
